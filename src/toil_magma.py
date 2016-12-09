8c00135d8f0e0aa2d4a40de276de206decba176a
Vassily Trubetskoy <trubetskoy.vasa@gmail.com>
2016-12-09 09:15:40 -0500

    Broke out some hardcoded variables into a command line interface.

--- src/toil_magma.py
+++ src/toil_magma.py
@@ -9,7 +9,10 @@ from tasks.MakeSnpLocationFile import MakeSnpLocationFile
 from tasks.TestGeneSets import TestGeneSets
 from tasks.MergeTestSets import MergeTestSets
 
-def run_magma_pipeline(toil_options):
+import argparse
+
+
+def run_magma_pipeline(toil_options, sample_size, daner_file):
     """
     Initial target: MAGMA analysis using summary statistics
     """
@@ -18,21 +21,20 @@ def run_magma_pipeline(toil_options):
     MAGMA_BINARY = BROAD_GIT_ROOT + "resources/magma_linux/magma"
     REF_1000G = "/psych/ripke/share/vasa/magma_ref/g1000_eur"
     REF_GENE_LOC = BROAD_GIT_ROOT + "resources/magma_macOS/reference_data/NCBI37.3.gene.loc"
-    DANER_FILE = BROAD_GIT_ROOT + "test/resources/pgc_scz_chr22_subset.daner"
-    SAMPLE_SIZE = "2000"
 
     # define jobs
-    make_snp_location_file_job = MakeSnpLocationFile(daner_file=DANER_FILE)
+    make_snp_location_file_job = MakeSnpLocationFile(daner_file=daner_file)
     annotate_summary_stats_job = AnnotateSummaryStats(magma_bin=MAGMA_BINARY,
                                                       snp_loc_file=make_snp_location_file_job.rv(),
                                                       gene_loc_ref=REF_GENE_LOC)
     test_gene_sets_jobs = [TestGeneSets(chromosome=str(chrm),
                                         magma_bin=MAGMA_BINARY,
-                                        sample_size=SAMPLE_SIZE,
+                                        sample_size=sample_size,
                                         annotated_file=annotate_summary_stats_job.rv(),
-                                        daner_file=DANER_FILE,
+                                        daner_file=daner_file,
                                         reference_data=REF_1000G) for chrm in range(22, 23)]
-    merge_gene_sets_job = MergeTestSets(magma_bin=MAGMA_BINARY, batch_results=[x.rv() for x in test_gene_sets_jobs])
+    merge_gene_sets_job = MergeTestSets(magma_bin=MAGMA_BINARY,
+                                        batch_results=[x.rv() for x in test_gene_sets_jobs])
 
     # specify dependencies among jobs
     make_snp_location_file_job.addChild(annotate_summary_stats_job)
@@ -45,10 +47,38 @@ def run_magma_pipeline(toil_options):
 
 
 if __name__ == "__main__":
-    toil_options = Job.Runner.getDefaultOptions('./test_file_store')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file-store",
+                        action="store",
+                        dest="file_store",
+                        default="./toil_file_store/",
+                        help="The location for the toil file store.")
+    parser.add_argument("--daner-file",
+                        action="store",
+                        dest="daner_file",
+                        help="""The location of the Daner file containing summary
+                        statistics""")
+    parser.add_argument("--sample-size",
+                        action="store",
+                        dest="sample_size",
+                        help="""The sample size of the study in which the summary statistics
+                        were generated.""")
+    parser.add_argument("--backend",
+                        action="store",
+                        dest="backend",
+                        choices=["singleMachine", "gridEngine"],
+                        help="""The backend which toil will execute jobs on. the
+                        'singleMachine' option means that the entire MAGMA workflow
+                        will be executed locally. The 'gridEngine' option means
+                        that jobs will be submitted to a cluster running SGE/OGE/UGE
+                        (such as the Broad's UGER).""")
+    args = parser.parse_args()
+
+    toil_options = Job.Runner.getDefaultOptions(args.file_store)
     toil_options.clean = 'always'
-    toil_options.clean = 'never'
-    toil_options.cleanWorkDir = 'never'
-    toil_options.workDir = '/home/unix/vassily/projects/pgc/ricopili_extension/toil_testing/toil_temp/'
-    toil_options.logLevel = 'INFO'
-    run_magma_pipeline(toil_options)
+    toil_options.logLevel = 'OFF'
+
+    run_magma_pipeline(toil_options,
+                       sample_size=args.sample_size,
+                       daner_file=args.daner_file)
