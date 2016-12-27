1904576cdf41e64a492debb0bad205254a5d24f7
Vassily Trubetskoy <trubetskoy.vasa@gmail.com>
2016-12-27 12:06:17 -0500

    Added config, snakemake, binaries and MAGMA resources to setuptools package_data. This allows me to correctly reference external resources within the repo.

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 
 from setuptools import setup, find_packages
 
-setup(name='ricopili_bioinformatics',
-      version='0.1',
-      description='Prototype for snakemake-based Ricopili postimputation pipeline.',
-      url='https://github.com/vtrubets/ricopili_bioinformatics',
-      author='Vasa Trubetskoy',
+setup(name="ricopili_bioinformatics",
+      version="0.1",
+      description="Prototype for snakemake-based Ricopili postimputation pipeline.",
+      url="https://github.com/vtrubets/ricopili_bioinformatics",
+      author="Vasa Trubetskoy",
       author_email='',
-      license='MIT',
+      license="MIT",
       packages=find_packages(),
-      install_requires=['snakemake'],
-      scripts=['src/snakemake/postimputation'],
+      install_requires=["snakemake"],
+      scripts=["src/snakemake/postimputation"],
+      package_data={"snakemake_config": "src/snakemake/config.yaml",
+                    "cluster_config": "src/snakemake/broad_uger_config.yaml",
+                    "magma_binary": "resources/magma_linux/magma",
+                    "gene_loc_ref": "resources/magma_linux/reference_data/"
+                                    "NCBI37.3.gene.loc",
+                    "snakefile": "src/snakemake/Snakefile"},
       zip_safe=False)
